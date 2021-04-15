dcdf90978d502a07a8e2bbc3be632fce442b1717
Matias Acosta <2277@holbertonschool.com>
2021-04-15 14:23:41 -0300

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,44 @@
 #!/usr/bin/python3
-'''Fabric module'''
-from fabric.api import env, run, put
-from os import path
+"""Deploy Server"""
 
-env.hosts = ['35.231.45.150', '34.74.42.82']
+
+from fabric.api import *
+from os import path
+from datetime import datetime
+
+
+env.hosts = ['34.75.78.32', '34.73.48.61']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    '''Distributes an archive to your web servers,
-    using the function do_deploy'''
-    filename = archive_path[9:-4]
-
-    if not path.exists(archive_path):
-        return False
-    try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        run('tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(filename + ".tgz", filename))
-        run('rm /tmp/{}'.format(filename + '.tgz'))
-        run('mv /data/web_static/releases/{}/web_static/* '
-            ' /data/web_static/releases/{}/'.format(filename, filename))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(filename))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(filename))
+    """Function that decompress and deploy functions in the serv"""
+    if path.exists(archive_path):
+        absFilePath = path.abspath(archive_path)
+        filepath, filename = path.split(absFilePath)
+        local = "{}".format(archive_path)
+        dest = "/tmp/{}".format(filename)
+        if put(local, dest).failed:
+            return False
+        folder = filename[:-4]
+        releases = "/data/web_static/releases"
+        if run('mkdir -p {}/{}'.format(releases, folder)).failed:
+            return False
+        if run('tar -xzf /tmp/{} -C {}/{}'.
+               format(filename, releases, folder)).failed:
+            return False
+        if run('rm /tmp/{}'.format(filename)).failed:
+            return False
+        if run('mv {}/{}/web_static/* {}/{}/'.
+               format(releases, folder, releases, folder)).failed:
+            return False
+        if run('rm -rf {}/web_static'.format(releases)).failed:
+            return False
+        if run('rm -rf /data/web_static/current').failed:
+            return False
+        if run('ln -s {}/{}/ /data/web_static/current'.
+               format(releases, folder)).failed:
+            return False
         return True
-    except:
+    else:
         return False
-    return False
