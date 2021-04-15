1ff3d5ab1904abaa61bce5add85d88bc2e42613a
Matias Acosta <2277@holbertonschool.com>
2021-04-15 16:00:17 -0300

    Add other funcs

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,16 +5,60 @@
 from fabric.api import *
 from os import path
 from datetime import datetime
-pack = __import__("1-pack_web_static").do_pack
-do_deploy = __import__("2-do_deploy_web_static").do_deploy
 
 
 env.hosts = ['34.75.78.32', '34.73.48.61']
 
 
+def do_pack():
+    """Function convert to tgz the content of web static folder"""
+    if not path.isdir('versions'):
+        if local("mkdir versions").failed:
+            return None
+    now = datetime.now()
+    formated = now.strftime("%Y%m%d%H%M%S")
+    filepath = "versions/web_static_{}.tgz".format(formated)
+    if local("tar -cvzf {} web_static".format(filepath)).failed:
+        return None
+    return filepath
+
+
+def do_deploy(archive_path):
+    """Function that decompress and deploy functions in the serv"""
+    if path.exists(archive_path):
+        absFilePath = path.abspath(archive_path)
+        filepath, filename = path.split(absFilePath)
+        local = "{}".format(archive_path)
+        dest = "/tmp/{}".format(filename)
+        if put(local, dest).failed:
+            return False
+        folder = filename[:-4]
+        releases = "/data/web_static/releases"
+        if run('mkdir -p {}/{}'.format(releases, folder)).failed:
+            return False
+        if run('tar -xzf /tmp/{} -C {}/{}'.
+               format(filename, releases, folder)).failed:
+            return False
+        if run('rm /tmp/{}'.format(filename)).failed:
+            return False
+        if run('mv {}/{}/web_static/* {}/{}/'.
+               format(releases, folder, releases, folder)).failed:
+            return False
+        if run('rm -rf {}/web_static'.format(releases)).failed:
+            return False
+        if run('rm -rf /data/web_static/current').failed:
+            return False
+        if run('ln -s {}/{}/ /data/web_static/current'.
+               format(releases, folder)).failed:
+            return False
+        return True
+    else:
+        return False
+
+
 def deploy():
     """Deploy new version in the Server"""
-    filepath = pack()
+    filepath = do_pack()
     if filepath:
         return do_deploy(filepath)
     else:
