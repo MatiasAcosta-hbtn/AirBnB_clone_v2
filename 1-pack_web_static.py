5a4af8f9bec6c6b3d7b96386af667b8db801019c
Matias Acosta <2277@holbertonschool.com>
2021-04-15 11:48:27 -0300

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-from fabric.api import *
+"""Fab Compress the content of web_static folder"""
 
-env.user = 'c328b64ca38f'
-env.hosts = ['4fd667c2.hbtn-cod.io']
+
+from fabric.api import *
+from os.path import isdir
+from datetime import datetime
+
 
 def do_pack():
-	run('echo "Hola"')
-
+    """Function convert to tgz the content of web static folder""""
+    if not isdir('versions'):
+        if local("mkdir versions").failed:
+            return None
+    now = datetime.now()
+    formated = now.strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz web_static".format(formated)
+    if local("tar -cvzf {}".format(path)).failed:
+        return None
+    return path
