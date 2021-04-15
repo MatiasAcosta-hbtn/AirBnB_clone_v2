bbf65b107cdd49d7326ebf20be51e5d51f83afa1
Matias Acosta <2277@holbertonschool.com>
2021-04-15 19:32:26 -0300

    Advanced Task

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,10 +11,13 @@ env.hosts = ['34.75.78.32', '34.73.48.61']
 
 
 def do_clean(number=0):
+    """Function to clean trash files"""
     number += 1
     if (number == 1):
-        local('ls -lht versions/| tail -n +2 | xargs rm -rf')
+        local('cd versions; ls -t | tail -n +2 | xargs rm -rf')
         run('cd /data/web_static/releases; ls -t | tail -n +2 | xargs rm -rf')
     else:
-        local('ls -lht versions/ | tail -n +{} | xargs rm -rf'.format(number))
-        run('cd /data/web_static/releases; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+        local('cd versions; ls -t | tail -n +{} | xargs rm -rf'.
+              format(number))
+        run('cd /data/web_static/releases; ls -t | tail -n +{} | xargs rm -rf'.
+            format(number))
