46903f42828e2e1f95d81c77c4f9cbb8b5e792e0
Matias Acosta <2277@holbertonschool.com>
2021-05-01 19:55:16 -0300

    Task 7 and 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3
+'''Flask Module'''
+
+
+from flask import Flask, abort, render_template
+from models import storage
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_cities():
+    """Display a HTML all Cities by states"""
+    states_list = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+    
+    
+
+@app.teardown_appcontext
+def close_session(self):
+    """Function that call close method"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
