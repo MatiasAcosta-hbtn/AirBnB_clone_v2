be54446ffa5c6651433e004d7f90d7fc4fa65c33
Matias Acosta <2277@holbertonschool.com>
2021-05-02 19:10:16 -0300

    Task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-'''Flask Module'''
-
+''' starts a Flask web application'''
 
 from flask import Flask, abort, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def list_cities():
-    """Display a HTML all Cities by states"""
-    states_list = storage.all("State")
+def cities_by_states():
+    """display all cities"""
+    states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
 def close_session(self):
-    """Function that call close method"""
+    """to close session"""
     storage.close()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
